b3312067090a9c27478db1756dd2a8b5969c1ae1
Lucas Prado <troyscripter@gmail.com>
2026-04-16 13:22:53 -0300

    feat(order-service): add RabbitMQ configuration with queue, exchange and binding

---
+++ order-service/src/main/java/com/example/order_service/configuration/RabbitMQConfiguration.java
@@ -0,0 +1,52 @@
+package com.example.order_service.configuration;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfiguration {
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    @Bean
+    public Queue orderQueue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public TopicExchange orderExchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding binding(Queue orderQueue, TopicExchange orderExchange) {
+        return BindingBuilder
+                .bind(orderQueue)
+                .to(orderExchange)
+                .with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter converter(){
+        return new JacksonJsonMessageConverter();
+    }
+
+    @Bean
+    public AmqpTemplate amqpTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+}
