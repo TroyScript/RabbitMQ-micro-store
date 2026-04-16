42377f8b6b0b9b53bb2b68d506a42adbd86f64e4
Lucas Prado <troyscripter@gmail.com>
2026-04-16 15:46:56 -0300

    feat(order-service): add OrdersController with orders endpoints

---
+++ order-service/src/main/java/com/example/order_service/controller/OrdersController.java
@@ -0,0 +1,23 @@
+package com.example.order_service.controller;
+
+import com.example.order_service.dto.OrderRequestDTO;
+import com.example.order_service.service.OrdersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/orders")
+@RequiredArgsConstructor
+public class OrdersController {
+    private final OrdersService ordersService;
+
+    @PostMapping("/post")
+    public ResponseEntity<OrderRequestDTO> post(@RequestBody OrderRequestDTO order){
+        ordersService.sendOrder(order);
+        return ResponseEntity.ok(order);
+    }
+}
