4eba2893a620c4b021ba93142db0c6d5dfc94b68
Lucas Prado <troyscripter@gmail.com>
2026-04-16 21:36:02 -0300

    refactor: remove OrderRequestDTO unnecessary argument and add input validation

--- order-service/src/main/java/com/example/order_service/dto/OrderRequestDTO.java
+++ order-service/src/main/java/com/example/order_service/dto/OrderRequestDTO.java
@@ -1,10 +1,14 @@
 package com.example.order_service.dto;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class OrderRequestDTO {
-    private int id;
-    private int quantity;
+    @NotBlank(message = "Product must be specified")
     private String productName;
+
+    @Min(value = 1, message = "Quantity must be at least 1")
+    private int quantity;
 }
