6f5474231cb3d6cfe5bc3e7034788d7c226d94d8
Lucas Prado <troyscripter@gmail.com>
2026-04-16 21:52:09 -0300

    fix(inventory-service): check if OrderRequestDTO received is valid

--- order-service/src/main/java/com/example/order_service/controller/OrdersController.java
+++ order-service/src/main/java/com/example/order_service/controller/OrdersController.java
@@ -2,6 +2,7 @@ package com.example.order_service.controller;
 
 import com.example.order_service.dto.OrderRequestDTO;
 import com.example.order_service.service.OrdersService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,7 +17,7 @@ public class OrdersController {
     private final OrdersService ordersService;
 
     @PostMapping("/post")
-    public ResponseEntity<OrderRequestDTO> post(@RequestBody OrderRequestDTO order){
+    public ResponseEntity<OrderRequestDTO> post(@RequestBody @Valid OrderRequestDTO order){
         ordersService.sendOrder(order);
         return ResponseEntity.ok(order);
     }
