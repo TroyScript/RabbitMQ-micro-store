607378c06f9114784ce6769a38ab541094577a0c
Lucas Prado <troyscripter@gmail.com>
2026-04-21 13:46:35 -0300

    feat(inventory-service): add full RabbitMQ configuration for independence

--- inventory-service/src/main/java/com/example/inventory_service/configuration/RabbitMQConfiguration.java
+++ inventory-service/src/main/java/com/example/inventory_service/configuration/RabbitMQConfiguration.java
@@ -1,17 +1,45 @@
 package com.example.inventory_service.configuration;
 
-import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RabbitMQConfiguration {
+    @Value("${rabbitmq.exchange.name}")
+    private String exchangeName;
+
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
     @Bean
-    public MessageConverter converter() {
+    public Queue orderQueue() {
+        return new Queue(queueName, true);
+    }
+
+    @Bean
+    public TopicExchange orderExchange() {
+        return new TopicExchange(exchangeName);
+    }
+
+    @Bean
+    public Binding binding(Queue orderQueue, TopicExchange orderExchange) {
+        return BindingBuilder
+                .bind(orderQueue)
+                .to(orderExchange)
+                .with(routingKey);
+    }
+
+    @Bean
+    public MessageConverter converter(){
         return new JacksonJsonMessageConverter();
     }
 
